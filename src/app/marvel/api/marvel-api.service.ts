4152c2834ee9b9fe9244c4b2080d3750842945a1
Daniel Candido <danielfelipec18@hotmail.com>
2019-07-28 18:20:03 -0300

    Implementação do Offset.

--- src/app/marvel/api/marvel-api.service.ts
+++ src/app/marvel/api/marvel-api.service.ts
@@ -10,9 +10,12 @@ export class MarvelApiService {
   API_KEY = '2e0f9262dd38510fa30e2c98441cb569';
   ENDPOINT = `characters?apikey=${this.API_KEY}&limit=100`;
   API_URL = `https://gateway.marvel.com/v1/public/${this.ENDPOINT}`;
-  HEROES: any;
+  HEROES = [];
+  HEROES2 : any;
   OFFSET = 0;
-  DATA: any;
+  TOTAL: any;
+  DATA: any
+  URLS = [];
 
   constructor(private http: HttpClient) {
     console.log("ApiMarvel");
@@ -20,28 +23,44 @@ export class MarvelApiService {
 
 
   getHeroes(){
-    if(this.HEROES){
-      return Promise.resolve(this.HEROES);
+    if(this.HEROES2){
+      return Promise.resolve(this.HEROES2);
     }
-
+  
     return new Promise(resolve => {
       this.http.get(this.API_URL+`&offset=${this.OFFSET}`)
         .subscribe(data => {
-          this.DATA = data;
-          console.log(this.DATA);
-          let total = this.DATA.data.total;
-          // while(total < total){
-          //   total = -100;
-          //   this.OFFSET = +100;
-          //   console.log(total);
-          //   this.http.get(this.API_URL+`&offset=${this.OFFSET}`).subscribe(data => {
-          //     this.HEROES = data;
-          //     console.log(this.HEROES);
-          //   })
-          // }
-          resolve(this.DATA);
-        })
+          this.HEROES2 = data;
+          // console.log(this.HEROES2);
+            resolve(this.HEROES2);
+       },
+       (error => {
+         console.log(error);
+       }))
     })        
+  }
+
+  getData(){
+     return new Promise(resolve => {
+       this.http.get(this.API_URL)
+       .subscribe(dataObj => {
+         this.DATA = dataObj;
+         for(let i= 0; i < 15; i++){
+          //  console.log(this.API_URL+`&offset=${100*i}`);
+           this.URLS.push(this.API_URL+`&offset=${100*i}`);
+           this.http.get(this.URLS[i])
+           .subscribe(heroes => {
+             this.HEROES.push(heroes);
+           })
+         }
+         console.log(this.HEROES);
+        //  console.log(this.URLS);
+         resolve(this.HEROES);
+       },
+       (error => {
+         alert("Erro inesperado Código Erro: "  +error.status + " Erro:" +  error.statusText  );
+       }))
+     })
   }
 
 }
--- src/app/marvel/characters/characters.component.ts
+++ src/app/marvel/characters/characters.component.ts
@@ -22,6 +22,8 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 export class CharactersComponent implements OnInit {
   public obj: any;
   public heroes: any;
+  public obj1: any;
+  public heroes1 = [];
   displayedColumns: string[] = ['image','name','description','stories'];
   dataSource = new MatTableDataSource(this.heroes);
   expandedElement = this.heroes;
@@ -31,6 +33,7 @@ export class CharactersComponent implements OnInit {
 
   constructor(private apiMarvel : MarvelApiService) { 
     this.getAllCharacters();
+    // this.getAllHeroes();
   }
 
   ngOnInit() {
@@ -45,7 +48,18 @@ export class CharactersComponent implements OnInit {
         this.dataSource = new MatTableDataSource(this.heroes);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
-        console.log(this.heroes);
+        // console.log(this.heroes);
+      })
+  }
+
+  getAllHeroes(){
+    this.apiMarvel.getData()
+      .then(data => {
+        this.obj1 = data;
+        // for(let i =0; i < 15; i++){
+        //   this.heroes1.push(this.obj.data.results)
+        // }
+        console.log(this.obj1);
       })
   }
 
